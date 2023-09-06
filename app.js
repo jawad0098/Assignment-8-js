86c644effe660c30cb4d731aca092ac4558c6fc6
jawad0098 <hassanjawad858@gmail.com>
2023-09-06 01:13:00 -0700

    TEST

---
+++ app.js
@@ -0,0 +1,256 @@
+// CHAPTER 3
+
+// Q.NO-1
+// Ans:
+// var age="25"
+// alert("My age is "+age);
+
+// Q.NO-2
+// Ans:
+// -------------------------------------------------------------------------------------------------
+
+// let visitorName = "john Doe";
+// let productTitle = "T-shirt";
+// let quantity = 4;
+// let store = "xyz clothing store";
+// document.write(visitorName + " ordered " + quantity + productTitle + " on " + store);
+// --------------------------------------------------------------------------------------------------
+
+// Q.No-3
+// Ans:
+// var birthYear = 1998;
+// alert("My birth year is " + birthYear);
+// document.write("My birth Year is " + birthYear);
+
+
+// Q.No-4
+// Ans:
+// var visitorName = prompt("please enter your name:");
+// var productTitle = prompt("Please enter the product title you want to order:")
+// var quantity = prompt("Please enter the quantity you want to order:")
+// var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+// document.write(message);
+
+// =======================================================================================================================================
+
+
+                                                    //    CHAPTER- 5
+// Q.NO-1
+// Ans:
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1+num2;
+// console.log (num1+num2);
+// alert(3 + 5);
+// document.write("Sum of 3 and 5 is 8")
+
+
+
+                                                    // Q.NO-2
+                                                    // Ans:
+//  subtraction    
+
+// var num1 = 3;
+// var num2 = 5;
+// var sub = num1-num2;
+// console.log (num1 - num2)
+// alert (3-5);
+
+// multiplication
+
+// var num1 = 3;
+// var num2 = 5;
+// var sub = num1*num2;
+// console.log (num1 * num2)
+// alert (3*5);
+
+// division
+
+// var num1 = 3;
+// var num2 = 5;
+// var sub = num1/num2;
+// console.log (num1 / num2)
+// alert (3/5);
+
+// modulus
+
+// var num1 = 3;
+// var num2 = 5;
+// var sub = num1%num2;
+// console.log (num1 % num2)
+// alert (3%5);
+
+                                                 // Q.NO-3
+                                                //  Ans:
+
+// A. Declare a variable
+// var myVaribale;
+
+// B. Value after variable declaration is:
+// var myVariable = 42;
+// document.write("Value after variable declaration is: " + myVariable );
+
+// C. Initialize the variable with some number.
+// var myVaribale = 5;
+
+// D. Show the value of variable in your browser like “Initial
+// value: 5”.
+
+// var myVariable = 4;
+// document.write("5:"+ myVariable);
+
+// E. Increment the variable
+// var myVariable = 5;
+// myVariable++;
+// document.write("Value after increment is: " + myVariable);
+
+// F. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// var myVariable = 5;
+// myVariable++;
+// document.write("Value after increment is: " + myVariable);
+
+// G.Add 7 to the variable.
+// myVariable +=7;
+
+// H. Show the value of variable in your browser like “Value after addition is: 13”.
+// var myVariable = 6;
+// myVariable +=7;
+// document.write("Value after addition is: " + myVariable);
+
+// I. Decrement the variable.
+// myVariable--; 
+
+// J. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+
+// var myVariable = 13;
+// myVariable --;
+// document.write("Value after decrement is:" + myVariable)
+
+// K. Show the remainder after dividing the variable’s value 
+// by 3
+
+// var myVariable = 13; 
+// var remainder = myVariable % 3;
+// document.write("Remainder after dividing by 3 is:"+myVariable)
+
+// L.Output : “The remainder is : 0”.
+// var myVariable = 12;
+// var remainder = myVariable % 3;
+
+// if (remainder === 0) {
+//     document.write("The remainder is:"+ remainder);
+// } else{
+//     document.write("The remainder is not 0.");
+// }
+
+                                              // Q.NO-4
+                                            //   Ans:
+
+// var ticketPrice = 600;
+// var totalTicket =5;
+// var totalCost =ticketPrice*totalTicket;
+// document.write("Total cost to buy " + totalTicket + " tickets to a movie is " +totalCost+"PKR")
+
+                                               // Q.NO-6
+                                            //    Ans:
+
+// var celsiusTemperature = 25;
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F<br>");
+// var fahrenheitTemperature2 = 77;
+// var celsiusTemperature2 = (fahrenheitTemperature2-32)*5/9;
+// document.write(fahrenheitTemperature2 + "°F is"+ celsiusTemperature2 + "°C");
+
+                                                // Q.NO-7
+                                                // Ans:
+
+// var priceItem =650
+// var priceIthem2=100
+// var quantityItem1 = 3;
+// var quantityItem2 = 5;
+// var shippingCharges = 10.99
+
+// var priceItem =650;
+// document.write("Price of item 1 is "+priceItem)
+
+// var quantityItem1=3;
+// document.write("Quantity of item 1 is "+quantityItem1)
+
+// var priceIthem2=100;
+// document.write("Price of item 2 is "+priceIthem2)
+
+// var quantityItem2=7;
+// document.write("Quantity of item 2 is "+quantityItem2)
+
+// var shippingCharges=100;
+// document.write("Shipping Charges "+shippingCharges)
+
+// var total = priceItem*quantityItem1 + priceIthem2*quantityItem2+shippingCharges;
+// document.write("Total cost of your order is "+total)
+
+
+                                                    // Q.NO-8
+                                                    // Ans:
+// var totalMark = 980;
+// document.write("Total Mark is "+totalMark)
+// var obtainMark = 804;
+// document.write("Marks Obtain: "+obtainMark)
+// var percentage = 804/980*100;
+// document.write("Percentage : "+percentage)
+
+
+                                                //  Q.NO-9
+                                                // Ans:
+
+//  var usDollar = 10;
+//  var saudiRiyal = 25;
+//  var pkr = usDollar * 307 + saudiRiyal * 81;
+//  document.write("Total Currency in PKR: "+pkr)
+
+
+                                            //    Q.NO-10
+                                            // Ans:
+
+// var a = 5
+// var b = a+5*10/2;
+// document.write("Result: "+ b)
+
+
+                                        //    Q.NO-11
+                                        // Ans:
+
+// var currentYear = 2023
+// document.write("Current Year: "+currentYear)
+// var birthYear = 1998
+// document.write("Birth Year: "+birthYear)
+// var age = currentYear-birthYear
+// document.write("Age: "+age)
+
+
+                                            //  Q.NO-12
+                                            // Ans:
+                        
+// var radius=20;
+// document.write("Radius of a circle:"+radius);
+// var circum = 2*3.142*20;
+// document.write("The circumference is: "+circum);
+// var area = 3.142 *20 *20;
+// document.write("The Area Is: "+area);
+
+
+                                            // Q.No-13
+                                            // Ans:
+    
+// var snack = "chocolate bars"
+// var age = 25;
+// var maxAge =65;
+// var amountPerDay = 3;
+// var remaingAge = maxAge-age;
+// var totalSnack = remaingAge*365*amountPerDay;
+// document.write("You will need " + totalSnack +" "+ snack + " chocolate chip the last you until the ripe old age of "+maxAge+".")
+
+
+                                            //    THE END
